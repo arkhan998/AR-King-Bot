0119097d93f79671fe6a12672b6f4b39b0867f67
Naveed Dogar <111642055+naveeddogar@users.noreply.github.com>
2023-06-02 09:12:15 +0500

    ♕ÀR-κιиg❧Ùpdätê By Øwñér

---
+++ setting.js
@@ -0,0 +1,26 @@
+nm=    AR-King-Bot
+
+sc =  type/jawa
+
+md = mode/node.js
+
+ln = leagues/en/ur/si
+
+tz Asia/
+
+
+heroku/buildlogs
+
+
+
+
+
+
+
+
+2373783.7Hsharking-bot
+
+DC jawa WhatsApp bot type node. js
+
+
+©copyright Under MIT License
